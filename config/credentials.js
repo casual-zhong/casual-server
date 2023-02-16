ef871d90126992f7e9872b2021905512cb4c0686
zhonggenfeng <1781507490@qq.com>
2023-02-16 17:39:10 +0800

    1

---
+++ config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+    qq:{
+        user:'1781507490@qq.com',
+        pass:'mfbxppkfjnldchdd'
+    }
+}
--- router/index.js
+++ router/index.js
@@ -1,11 +1,20 @@
 var dbserver = require('../dao/dbserver');
+// 引入邮箱发送方法
+var emailserver = require('../dao/emailserver');
 
 module.exports = function(app){
-    app.get('/test', (req, res) => {
-       dbserver.findUser(res);      
-    });
+   app.get('/test', (req, res) => {
+      dbserver.findUser(res);      
+   });
 
-    app.get('/test1', (req, res) => {
-        dbserver.findFriend(res);      
-     });
+   app.get('/test1', (req, res) => {
+      dbserver.findFriend(res);      
+   });
+
+   // 邮箱测试
+   app.post('/mail',(req,res) =>{
+      let mail = req.body.mail;
+      emailserver.emailSignUp(mail,res)
+      // res.send(mail)
+   })
 }
