b27245c986b0f971e4c7faa71fab63e12ceb0d73
zhonggenfeng <1781507490@qq.com>
2023-02-16 17:39:10 +0800

    1

---
+++ dao/emailserver.js
@@ -0,0 +1,38 @@
+// 引用发送邮件插件
+var nodemailer = require('nodemailer');
+// 引入证书文件
+var credentials = require('../config/credentials');
+
+// 创建传输方式
+var transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:credentials.gmail.user,
+        pass:credentials.gmail.pass
+    }
+})
+
+// 注册发送邮件给用户
+exports.emailSignUp = function(email,res){
+    // 发送信息内容
+    let options = {
+        from:credentials.gmail.user,
+        to:email,
+        subject:'感谢您的注册',
+        html:`<span>测试</span>`,
+        attachments: [{
+            filename: 'aaa.png',
+            path: './aaa.png',
+            cid: ''
+        }]
+    }
+
+    // 发送邮件
+    transporter.sendMail(options,function(err,msg){
+        if(err){
+            res.send(err)
+        }else{
+            res.send('邮件发送成功！')
+        }
+    })
+}
--- index.js
+++ index.js
@@ -3,25 +3,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!') )
+// 引入解析req.body插件
+// var bodyParser = require('body-parser');
 
 // 解决跨域问题
 app.all("*",function(req,res,next){
     // 设置允许跨域的域名,*代表允许任意域名跨域
     res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Content-Type');
-    // res.header('Access-Control-Allow-Methods','DELETE,PUT,POST,GET,OPTIONS');
-    res.header('Access-Control-Allow-Methods','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS');
+    res.header('X-Powered-By',' 3.2.1');
     res.header('Content-Type','application/json; charset=utf-8');
-    next();
-    // if(req.method.toLowerCase() == 'options')
-    //     res.send(200); // 让options 尝试请求快速结束
-    // else
-    //     next();
+    if(req.method == 'OPTIONS') {
+        // 让options请求快速返回
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 })
 
-require('./router/index')(app);
+// 解析前端数据
+// app.use(bodyParser.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+require('./router/index')(app);
 
 // 404页面
 app.use(function(req,res,next){
