b9022d5982b11d2646510a41cc21aa0465b5dbe4
钟根凤 <1781507490@qq.com>
2024-03-07 15:01:40 +0800

    啊

--- router/index.js
+++ router/index.js
@@ -3,9 +3,12 @@
 var emailserver = require('../dao/emailserver');
 
 module.exports = function(app){
-   // app.get('/test', (req, res) => {
-   //    dbserver.findUser(res);      
-   // });
+   app.get('/test', (req, res) => {
+      res.send({
+         message:"返回成功"
+      })
+      // dbserver.findUser(res);      
+   });
 
    // app.get('/test1', (req, res) => {
    //    dbserver.findFriend(res);      
