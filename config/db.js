b8ef76cdfa10ce223f9805f8a94acbf19c913369
钟根凤 <1781507490@qq.com>
2024-12-11 16:32:21 +0800

    1

--- config/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var db = mongoose.createConnection('mongodb://localhost:27017/zhong',{ useNewUrlParser: true,useUnifiedTopology:true });
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.info('数据库连接成功！')
-});
-
-module.exports = db
